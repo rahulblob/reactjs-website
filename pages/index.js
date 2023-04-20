ec8c0f954eb1cd8168e94f60b6f4676453fb6c73
Rahul Lodhi <123192165+rahulblob@users.noreply.github.com>
2023-04-21 01:25:34 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
     <>
       <Head>
         <meta name='robots' content='index' />
+        <title>Contact us - Syntx</title>
       </Head>
       <Hero />
       <LogoGrid />
